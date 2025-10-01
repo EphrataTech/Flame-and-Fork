ca711321522c1c81e8e73b4a001a6184b89c1ef6
Eric Theuri <tericmail001@gmail.com>
2025-10-01 14:56:01 +0300

    Changed AI model to Groq

--- chatbot/backend/app/main.py
+++ chatbot/backend/app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI,status, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from .chatbot import Gemini
-from .models import ChatRequest, ChatResponse
+# from .chatbot import Gemini
+from models import ChatRequest, ChatResponse
 from dotenv import load_dotenv
+from langchain_groq import ChatGroq
 import logging
 import os
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,63 +17,61 @@ try:
 except Exception as e:
     logger.critical(f"Failed to load .env file: {e}")
     raise RuntimeError("Could not configure the AI service: {e}")
-api_key = os.getenv("API_KEY")
 #App initialization
+
 try:
-    app = FastAPI()
+    app = FastAPI(title = "Flame and Fork",description="Flame and Fork Chatbot",version="1.1" )
     logger.info("FastAPI app initialized")
 except Exception as e:
     logger.critical(f"Failed to initialize fastAPI app: {e}")
     raise
 
 # CORS configuration
-try:
-    app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500",
+
+origins = origins=["http://127.0.0.1:5500",
             "http://localhost:5501",
             "http://127.0.0.1:5501",
             "http://localhost:8000",
             "http://127.0.0.1:8000",
             "https://flameandfork.com",
             "http://localhost:5000",
-            "https://flame-and-fork.netlify.app",],
+            "https://flame-and-fork.netlify.app",]
+try:
+    app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origins],
     allow_credentials=False,
-    allow_methods=["POST", "GET","OPTIONS"],
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 except Exception as e:
     logger.error(f"Failed to configure CORS: {e}")
     raise
-#Initialize AI platform
 
 try:
-    if not api_key:
-        logger.critical("Could not get the env variable")
-        raise ValueError("API key is not available")
-    ai_platform = Gemini(api_key)
-    logger.info("AI platform started successfully")
+    ai_model = os.getenv("GROQ_MODEL")
+    groq_api_key = os.getenv("GROQ_API_KEY")
+    ai_platform = ChatGroq(model=ai_model,api_key=groq_api_key)
+    logger.info("Successfully initiated the AI model")
 except Exception as e:
-    logger.critical(f"Failed to initialize AI platform: {e}")
+    logger.exception("Failed to initialize the AI model")
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Chatbot API"}
 
 @app.post("/chat", response_model=ChatResponse)
-async def chatbot(request: ChatRequest):
+async def chat(request: ChatRequest):
     try:
-        response_text = ai_platform.chat(request.prompt)
+        response_text = ai_platform.invoke(request.prompt).content
+        if response_text:
+            logger.info("Chatbot returned an answer!")
         return ChatResponse(response=response_text)
-    except RuntimeError as e:
-        logger.error(f"The chatbot has encountered an error: {e}")
-        raise HTTPException(status_code=503, detail="Chatbot temporarily unavailable. Please try again later.")
     except Exception as e:
-        logger.error(f"Unexpected error: {e}")
-        raise HTTPException(status_code=500, detail="Unexpected server error.")
-    #return ChatResponse(response={request.prompt}")
+        logger.exception("The chatbot failed to respond.")
+
 
 
 if __name__=="__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8003, reload=True)
